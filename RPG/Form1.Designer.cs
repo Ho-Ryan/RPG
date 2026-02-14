8079b562f8e8b44d98b9eae1874cb8c583edc202
Jian Ho Ryan <horyan.200918@gmail.com>
2026-02-14 12:47:30 +0100

    Ajout de division

--- RPG/Form1.Designer.cs
+++ RPG/Form1.Designer.cs
@@ -29,10 +29,15 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
+            Actions = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            Events = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -41,67 +46,100 @@
             label1.Font = new Font("Calibri", 20F, FontStyle.Bold);
             label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(108, 33);
+            label1.Size = new Size(100, 33);
             label1.TabIndex = 0;
-            label1.Text = "Worlds :";
+            label1.Text = "Action :";
             // 
-            // label2
+            // Actions
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Calibri", 20F, FontStyle.Bold);
-            label2.Location = new Point(12, 124);
-            label2.Name = "label2";
-            label2.Size = new Size(114, 33);
-            label2.TabIndex = 1;
-            label2.Text = "2) Forest";
+            Actions.AutoSize = true;
+            Actions.Font = new Font("Calibri", 20F, FontStyle.Bold);
+            Actions.Location = new Point(12, 91);
+            Actions.Name = "Actions";
+            Actions.Size = new Size(0, 33);
+            Actions.TabIndex = 4;
             // 
-            // label3
+            // pictureBox1
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Calibri", 20F, FontStyle.Bold);
-            label3.Location = new Point(12, 157);
-            label3.Name = "label3";
-            label3.Size = new Size(108, 33);
-            label3.TabIndex = 2;
-            label3.Text = "3) Caves";
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Location = new Point(790, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(10, 900);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
             // 
-            // label4
+            // pictureBox2
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Calibri", 20F, FontStyle.Bold);
-            label4.Location = new Point(12, 190);
-            label4.Name = "label4";
-            label4.Size = new Size(104, 33);
-            label4.TabIndex = 3;
-            label4.Text = "4) Ruins";
+            pictureBox2.BackColor = Color.White;
+            pictureBox2.Location = new Point(0, 440);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(1600, 10);
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
             // 
-            // label5
+            // label6
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Calibri", 20F, FontStyle.Bold);
-            label5.Location = new Point(12, 91);
-            label5.Name = "label5";
-            label5.Size = new Size(99, 33);
-            label5.TabIndex = 4;
-            label5.Text = "1) Shop";
+            label6.AutoSize = true;
+            label6.Font = new Font("Calibri", 20F, FontStyle.Bold);
+            label6.Location = new Point(12, 463);
+            label6.Name = "label6";
+            label6.Size = new Size(143, 33);
+            label6.TabIndex = 7;
+            label6.Text = "Inventory  :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Calibri", 20F, FontStyle.Bold);
+            label7.Location = new Point(816, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(108, 33);
+            label7.TabIndex = 8;
+            label7.Text = "Worlds :";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Calibri", 20F, FontStyle.Bold);
+            label8.Location = new Point(816, 463);
+            label8.Name = "label8";
+            label8.Size = new Size(101, 33);
+            label8.TabIndex = 9;
+            label8.Text = "Events :";
+            // 
+            // Events
+            // 
+            Events.BackColor = SystemColors.InactiveCaptionText;
+            Events.BorderStyle = BorderStyle.FixedSingle;
+            Events.Font = new Font("Calibri", 12F);
+            Events.Location = new Point(816, 496);
+            Events.Name = "Events";
+            Events.Size = new Size(756, 356);
+            Events.TabIndex = 10;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
-            ClientSize = new Size(884, 561);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
+            ClientSize = new Size(1584, 861);
+            Controls.Add(Events);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(Actions);
             Controls.Add(label1);
             ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "RPG";
             Load += Form1_Load;
             KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -109,9 +147,12 @@
         #endregion
 
         private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
+        private Label Actions;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label Events;
     }
 }
--- RPG/Form1.cs
+++ RPG/Form1.cs
@@ -2,6 +2,7 @@ namespace RPG
 {
     public partial class Form1 : Form
     {
+        public bool InAction = false;
         public Form1()
         {
             InitializeComponent();
@@ -9,24 +10,42 @@ namespace RPG
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             Player player = new Player();
             Ennemy ennemies = new Ennemy();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            //Selection de zone
-            switch (e.KeyData)
+            //Selection de zone$
+            if (!InAction)
             {
-                case Keys.D1:
-                    break;
-                case Keys.D2:
-                    break;
-                case Keys.D3:
-                    break;
-                case Keys.D4:
-                    break;
+                switch (e.KeyData)
+                {
+                    case Keys.D1:
+                        Shop();
+                        break;
+                    case Keys.D2:
+                        InAction = true;
+                        Forest();
+                        break;
+                    case Keys.D3:
+                        break;
+                    case Keys.D4:
+                        break;
+                }
             }
+        }
+
+        public void Forest()
+        {
+            Events.Text += ("Vous aller dans la forêt\n");
+
+        }
+
+        public void Shop()
+        {
+            Events.Text += ("Vous entrez dans un shop\n");
         }
     }
 }
